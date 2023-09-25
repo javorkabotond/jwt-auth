a5d8c067362ea8c3aa215c59236523e657c0c026
Botond Javorka <javorka.botond@gmail.com>
2023-09-25 11:20:14 +0200

    valami

--- src/main/java/com/javorkabotond/jwtauth/JwtAuthApplication.java
+++ src/main/java/com/javorkabotond/jwtauth/JwtAuthApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JwtAuthApplication {
 
+    /**
+     * Main entry point.
+     *
+     * @param args arguments
+     */
     public static void main(String[] args) {
 
         SpringApplication.run(JwtAuthApplication.class, args);
